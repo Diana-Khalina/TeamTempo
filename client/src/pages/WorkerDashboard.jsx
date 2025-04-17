901caba98ca71c6bf17c799f69f76ecba2c20c5c
Mohamed Abdi <mabdi@Mohameds-MBP.lan>
2025-04-16 19:27:04 -0500

    frontendcode

--- client/src/pages/WorkerDashboard.jsx
+++ client/src/pages/WorkerDashboard.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
-const WorkerDashboard = () => {
-  return <div class="container">
-    <Navigation /> 
+import { Link } from 'react-router-dom';
+import './WorkerDashboard.css'; // <- Import the styles
 
-    <h1>Welcome to Worker Dashboard</h1>
-    </div>;
+const WorkerDashboard = () => {
+  return (
+    <div class="container">
+      <Navigation />
+      <h1>Welcome to Worker Dashboard</h1>
+      
+      <nav className="navbar-links">
+      <a href="/WorkerDashboard.jsx" className="navbar-link main">Main</a>
+      <a href="/tracker" className="navbar-link tracker">Tracker</a>
+      </nav>
+
+    </div>
+  );
 };
 
 export default WorkerDashboard;
